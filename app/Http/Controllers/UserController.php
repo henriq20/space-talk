918e398812cf221cb340cbad21e4fd63458a5dc8
henriq20 <henrique-ferreira20@hotmail.com>
2021-09-26 14:06:04 -0300

    Return empty json when making ajax call

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,19 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function authenticate()
+    public function authenticate(LoginRequest $request)
     {
-        
+
     }
 
     public function store(RegisterRequest $request)
     {
+        // Returns an empty JSON if the request is an AJAX call.
+        if ($request->wantsJson() || $request->ajax()) {
+            return response()->json();
+        }
+        
         $user = new User($request->input());
+        $user->password = Hash::make($request->input('password'));
         $user->save();
 
         return redirect('/login')->
