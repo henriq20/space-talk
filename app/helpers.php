f813893965b5a4e45847fefcd62b84dd9c88bd29
henriq20 <henrique-ferreira20@hotmail.com>
2021-10-09 13:56:27 -0300

    Create function to abbreaviate large numbers

--- app/helpers.php
+++ app/helpers.php
@@ -10,3 +10,22 @@ function user(): User
 {
     return auth()->user();
 }
+
+/**
+ * Gets the abbreviated version of the number.
+ * @param string|int|float $number
+ * @return string The abbreviated number.
+ */
+function abbreaviate($number)
+{
+    if ($number < 1_000) {
+        return $number;
+    }
+
+    $suffix = $number >= 1_000_000 ? 'm' : 'k';
+    $number /= $suffix === 'm' ? 1_000_000 : 1_000;
+    
+    // floatval() to remove unwanted decimal zeros
+    // substr() to only get 1 decimal place
+    return floatval(substr($number, 0, strpos($number, '.') + 2)) . $suffix;
+}
--- resources/views/inc/post-aside.blade.php
+++ resources/views/inc/post-aside.blade.php
@@ -2,7 +2,7 @@
     <button class="upvote btn btn-transparent" data-action="upvote">
         <i class="fas fa-arrow-up text-dark @auth @if (auth()->user()->upvoted($post)) upvoted @endif @endauth"></i>
     </button>
-    <div class="votes">{{ $post->totalVotes() }}</div>
+    <div class="votes">{{ abbreaviate($post->totalVotes()) }}</div>
     <button class="downvote btn btn-transparent" data-action="downvote">
         <i class="fas fa-arrow-down text-dark @auth @if (auth()->user()->downvoted($post)) downvoted @endif @endauth"></i>
     </button>
